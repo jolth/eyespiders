f262c54264055bbaec8bc4e228f1b3bba3c1bc50
Jorge Toro <jolthgs@gmail.com>
2012-05-10 10:36:40 -0500

    update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 
 __author__    = "Jorge Alonso Toro (jolth) [jolthgs@gmail.com]"
-__version__   = "1.0"
+__version__   = "0.0.1"
 __date__      = "28 feb 2012"
 __copyright__ = "Copyright (c) 2012 Jorge A. Toro"
 __license__   = "PSFL"
